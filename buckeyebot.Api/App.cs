f056ea0d2200c5d4ede1e8676a4e901cdc5c68b0
Jiaqi Song <songnosekai@gmail.com>
2024-10-29 18:32:30 -0400

    first commit

---
+++ buckeyebot.Api/App.cs
@@ -0,0 +1,20 @@
+using jet.piranha.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace buckeyebot.Api
+{
+public class Program
+{
+public static void Main(string[] args)
+{
+CreateHostBuilder(args).Build().Run();
+}
+public static IHostBuilder CreateHostBuilder(string[] args) =>
+Host.CreateDefaultBuilder(args)
+.ConfigureWebHostDefaults(webBuilder =>
+{
+webBuilder.UseStartup<Startup>();
+});
+}
+}
